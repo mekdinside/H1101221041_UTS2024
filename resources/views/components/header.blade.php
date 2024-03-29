0ff41dc07d2d379c981690a5bb8f2f4d759dfcdd
mekdinside <mericynthia00@gmail.com>
2024-03-29 23:22:27 +0700

    judul header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -50,6 +50,7 @@
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <h1 class="text-white">SI Kepegawaian</h1>
           </ol>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
